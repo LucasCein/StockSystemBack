fd9825b5457447b6ef6abca7e9733a4a3979585e
Lucas Cein <lucascein82@gmail.com>
2023-11-19 19:45:10 -0300

    paso a produccion fix3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const productosRoutes = require('./productoCRUD');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 
 // Middleware para parsear JSON
