b2bdbb78c35e9f08d0b7e11868178ec53988d8f3
Lucas Cein <lucascein82@gmail.com>
2023-11-30 15:00:02 -0300

    Eliminar vencidos fix

--- scheduledTasks.js
+++ scheduledTasks.js
@@ -1,4 +1,14 @@
 const cron = require('node-cron');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
 const eliminarProductosAntiguos = async () => {
     try {
         const result = await pool.query(
@@ -10,6 +20,5 @@ const eliminarProductosAntiguos = async () => {
     }
 };
 
-// cron.schedule('0 0 * * *', eliminarProductosAntiguos);
+cron.schedule('0 0 * * *', eliminarProductosAntiguos);
 
-setTimeout(eliminarProductosAntiguos,120000)
