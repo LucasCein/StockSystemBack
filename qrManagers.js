e7a387b181c9ff72181bfbfb01854a3549fd9f48
Lucas Cein <lucascein82@gmail.com>
2023-12-01 14:14:38 -0300

    Qr modificacion

--- qrManagers.js
+++ qrManagers.js
@@ -11,13 +11,22 @@ function generarURLUnica(idProducto) {
 async function generarQR(idProducto) {
     try {
         const url = generarURLUnica(idProducto);
-        const qrCode = await QRCode.toDataURL(url);
+        const qrOptions = {
+            scale: 10, // Ajusta este valor según sea necesario
+            margin: 2,
+            color: {
+                dark: "#000000",  // Puntos del QR
+                light: "#FFFFFF" // Fondo del QR
+            }
+        };
+        const qrCode = await QRCode.toDataURL(url, qrOptions);
         return qrCode;
     } catch (err) {
         console.error(err);
         throw new Error('Fallo al generar el código QR');
     }
 }
+
 
 
 module.exports = {
