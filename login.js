e701a646e8570aacc093fea87a88522a53cd9f5b
Lucas Cein <lucascein82@gmail.com>
2023-12-04 18:18:14 -0300

    login fix3

--- login.js
+++ login.js
@@ -20,17 +20,19 @@ const pool = new Pool({
 //         res.status(500).send(err.message);
 //     }
 // });
+// En tu archivo del backend
 router.post('/login', async (req, res) => {
-    try {
-        const { name, password } = req.body;
-        const result = await pool.query('SELECT * FROM users WHERE name = $1 AND password = $2', [name, password]);
-        if (result.rows.length === 0) {
-            return res.status(401).send('Credenciales incorrectas');
-        }
-        res.send('Login exitoso');
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+  try {
+      const { name, password } = req.body;
+      const result = await pool.query('SELECT * FROM users WHERE name = $1 AND password = $2', [name, password]);
+      if (result.rows.length === 0) {
+          return res.status(401).json({ message: 'Credenciales incorrectas' });
+      }
+      res.json({ message: 'Login exitoso' });
+  } catch (err) {
+      res.status(500).json({ message: err.message });
+  }
 });
+
 
 module.exports = router;
