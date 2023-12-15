7a7867877ef1f4f0547a9a277305d7b446d7c95e
Lucas Cein <lucascein82@gmail.com>
2023-12-15 12:50:06 -0300

    Update finales

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(express.json());
 
 
 //Configuración de CORS
-// const corsOptions = {
-//   origin: 'https://qrsystemfront.onrender.com', // Asegúrate de que este sea el dominio de tu frontend
-//   optionsSuccessStatus: 200 // Para navegadores antiguos que no soportan 204
-// };
 const corsOptions = {
-  origin: 'http://localhost:5173', // Asegúrate de que este sea el dominio de tu frontend
+  origin: 'https://qrsystemfront.onrender.com', // Asegúrate de que este sea el dominio de tu frontend
   optionsSuccessStatus: 200 // Para navegadores antiguos que no soportan 204
 };
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // Asegúrate de que este sea el dominio de tu frontend
+//   optionsSuccessStatus: 200 // Para navegadores antiguos que no soportan 204
+// };
 // Aplica CORS antes de tus rutas
 app.use(cors(corsOptions));
 
@@ -35,9 +35,9 @@ app.use(loginRoutes);
 
 // Inicia el servidor
 const PORT = process.env.PORT || 3000;
-// app.listen(PORT, '::', () => {
-//   console.log(`Servidor corriendo en https://qrsystemback.onrender.com:${PORT}`);
-// });
 app.listen(PORT, '::', () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en https://qrsystemback.onrender.com:${PORT}`);
 });
+// app.listen(PORT, '::', () => {
+//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
