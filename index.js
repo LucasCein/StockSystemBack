b0d3448e0565db858df8c665de9310b5a8a35745
Lucas Cein <lucascein82@gmail.com>
2024-01-14 20:14:25 -0300

    prod

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ app.use(express.json());
 
 //Configuración de CORS
 const corsOptions = {
-  origin: 'https://stocksystemfront.onrender.com', // Asegúrate de que este sea el dominio de tu frontend
+  origin: function (origin, callback) {
+    const allowedOrigins = ['http://localhost:5173', 'https://stocksystemfront.onrender.com'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200 // Para navegadores antiguos que no soportan 204
 };
-// const corsOptions = {
-//   origin: 'http://localhost:5173', // Asegúrate de que este sea el dominio de tu frontend
-//   optionsSuccessStatus: 200 // Para navegadores antiguos que no soportan 204
-// };
+
+app.use(cors(corsOptions));
+
 // Aplica CORS antes de tus rutas
 app.use(cors(corsOptions));
 
