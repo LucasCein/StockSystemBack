7d0049401410b0cf47b0c31fba912a01ea6d3b06
LucasCein <91496200+LucasCein@users.noreply.github.com>
2024-05-18 22:20:35 -0300

    Update excelStorage.js

--- excelStorage.js
+++ excelStorage.js
@@ -3,49 +3,46 @@ const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
-const router=express.Router();
+const router = express.Router();
 
-const FILE_LIFETIME = 60 * 60 * 1000
+const FILE_LIFETIME = 60 * 60 * 1000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const uploadPath = path.join(__dirname, "/uploads/");
-    fs.mkdirSync(uploadPath, { recursive: true }); // Asegúrate de que el directorio exista
+    fs.mkdirSync(uploadPath, { recursive: true });
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    const fileId = uuidv4(); // Crea un UUID para el archivo
-    cb(null, fileId + path.extname(file.originalname)); // Guarda el archivo con el UUID como nombre
+    const fileId = uuidv4();
+    cb(null, fileId + path.extname(file.originalname));
   },
 });
 
 const upload = multer({ storage: storage });
 
-router.post("/", upload.single("file"), (req, res) => {
-  res.json({ message: "Archivo subido con éxito", fileId: req.file.filename });
+router.post("/", upload.array("files", 10), (req, res) => {
+  const fileInfos = req.files.map(file => ({ message: "Archivo subido con éxito", fileId: file.filename }));
+  res.json(fileInfos);
 });
 
 router.post('/json', (req, res) => {
   const { headers, data } = req.body;
-  const fileId = Date.now(); // Generar un ID único para el archivo
+  const fileId = Date.now();
 
-  // Crear el contenido del archivo JSON
   const jsonData = {
     headers: headers,
     data: data
   };
 
-  // Definir la ruta del archivo
   const filePath = path.join(__dirname, `data_${fileId}.json`);
 
-  // Guardar el archivo JSON
   fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), (err) => {
     if (err) {
       console.error('Error saving file:', err);
       return res.status(500).send('Internal Server Error');
     }
 
-    // Programar la eliminación del archivo después de FILE_LIFETIME milisegundos
     setTimeout(() => {
       fs.unlink(filePath, (err) => {
         if (err) {
@@ -73,16 +70,16 @@ router.get('/json/:fileId', (req, res) => {
 });
 
 router.get('/:fileId', (req, res) => {
-    const fileId = req.params.fileId;
-    const filePath = path.join(__dirname, '/uploads/', fileId);
-  
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.status(404).send('Archivo no encontrado');
-        return;
-      }
-      res.send(data); // Envía los datos del archivo como respuesta
-    });
-  });
+  const fileId = req.params.fileId;
+  const filePath = path.join(__dirname, '/uploads/', fileId);
 
-module.exports=router
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.status(404).send('Archivo no encontrado');
+      return;
+    }
+    res.send(data);
+  });
+});
+
+module.exports = router;
